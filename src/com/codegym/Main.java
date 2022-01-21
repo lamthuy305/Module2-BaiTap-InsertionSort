59e5835681862de4e36a726bea4bc2c1412b4be2
lamthuy305 <lamthuy305@gmail.com>
2022-01-21 16:19:13 +0700

    done

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,24 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+        insertionSort(list);
+        for (int i = 0; i < list.length; i++)
+            System.out.print(list[i] + " ");
+    }
+
+    public static void insertionSort(int[] array) {
+        int pos, x;
+        for (int i = 1; i < array.length; i++) {
+            x = array[i];
+            pos = i;
+            while (pos > 0 && x < array[pos - 1]) {
+                array[pos] = array[pos - 1];
+                pos--;
+            }
+            array[pos] = x;
+        }
+    }
+}
